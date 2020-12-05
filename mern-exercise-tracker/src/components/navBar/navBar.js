e153edbd28593ad1a3a70e29eecba81606b6844e
danlee0528 <danlee0528@hotmail.com>
2020-12-04 22:17:50 -0800

    fix backend database connection error

--- mern-exercise-tracker/src/components/navBar/navBar.js
+++ mern-exercise-tracker/src/components/navBar/navBar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
         return ( 
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
                 <Link to="/" className="navbar-brand">Exercise Tracker</Link>
-                <div className="collapse navbar-collapse">
+                <div className="collpase navbar-collapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="navbar-item">
                             <Link to="/" className="nav-link">Exercise</Link>
